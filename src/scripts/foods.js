52ce0f86183ef4486fcf66168d61e3b4bf8a3095
Jake Miller <jake0miller@gmail.com>
2019-10-14 13:37:20 -0600

    Add food view and buttons for back, edit, delete

--- src/scripts/foods.js
+++ src/scripts/foods.js
@@ -4,18 +4,37 @@ $(document).ready(function() {
 
     fetch('https://calorie-tracker-be.herokuapp.com/api/v1/foods', {
       method: 'get',
-      headers: {
-        "Content-Type": "application/json"
-      }
+      headers: {"Content-Type": "application/json"}
     })
     .then(response => response.json())
     .then(response => {
       $.each (response, function (index) {
-        var start = `<div class='entry' style='margin: 10px;'>\n`
-        var item = `<h2>${response[index].name}</h2>`
-        var cals = `<li>Cals: ${response[index].calories}</li>`
-        var close = `</div>`
-        $("#foods").append(start + item + cals + close)
+        let start = `<div class='entry' style='margin: 10px;'>\n`
+        let link = `<h2><a class='food-show' id='${'food-'+response[index].id}' href='javascript:;'>`
+        let item = `${response[index].name}</a></h2>`
+        let cals = `<li>Cals: ${response[index].calories}</li>`
+        let close = `</div>`
+        $("#foods").append(start + link + item + cals + close)
+      })
+
+      $(".food-show").on("click", function() {
+        let id = $(this).attr('id').split('-')[1];
+        fetch(`https://calorie-tracker-be.herokuapp.com/api/v1/foods/${id}`, {
+          method: 'get',
+          headers: {"Content-Type": "application/json"}
+        })
+        .then(response => response.json())
+        .then(response => {
+          let start = `<div class='entry' style='margin: 10px;'>\n`
+					let item = `<h2>${response.name}</h2>\n`
+					let cals = `<li>Cals: ${response.calories}</li>`
+          let close = `</div>`
+					let backBtn = '<button type="button" id="foods-index">Back</button>'
+					let deleteBtn = '<button type="button" id="delete-food">Delete</button>'
+					let editBtn = '<button type="button" id="edit-food">Edit</button>'
+					$('#foods').empty().append(start + item + cals + backBtn + editBtn + deleteBtn + close)
+        })
+        .catch(error => console.log(error))
       })
     })
     .catch(error => console.log(error))
