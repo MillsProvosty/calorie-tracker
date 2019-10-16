797793cef934e14d38ba7c8a0775342984c44b75
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-10-16 14:20:14 -0600

    Add devServer object to webpack.common.js

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -7,6 +7,13 @@ module.exports = {
   entry: {
     app: Path.resolve(__dirname, '../src/scripts/index.js')
   },
+  devServer: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
+    compress: true,
+    inline: true,
+    disableHostCheck: true
+  },
   output: {
     path: Path.join(__dirname, '../build'),
     filename: 'js/[name].js'
